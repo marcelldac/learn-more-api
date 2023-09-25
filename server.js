2df2d2f7152056fbdf26edca9488b25c15e83930
marcelldac <dacmarcell@proton.me>
2023-09-24 23:13:20 -0300

    feat: add health router

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const app = express();
 const PORT = 3000;
 
 const userRouter = require('./router/user');
+const healthRouter = require('./router/health');
 
 app.use(cors());
 app.use(express.json());
 app.use('/users', userRouter);
+app.use('/health', healthRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
