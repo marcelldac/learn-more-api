b2a06920d5f58ac011ca1de85b053e992a5e45ec
marcelldac <dacmarcell@proton.me>
2023-09-24 22:52:19 -0300

    feat: add crud

--- server.js
+++ server.js
@@ -1,21 +1,117 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { PrismaClient } = require('@prisma/client');
+
+const PORT = 3000;
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-  res.json('Hello World!');
+  res.json('ok');
 })
 
-//#region get all users
-app.get('/users', (req, res) => {
-  res.status(200).json("all users")
+//#region Read Users
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.status(200).json({ users });
+})
+
+/* Todo: read user by id */
+app.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const newId = parseInt(id);
+
+  if (!newId) {
+    return res.status(400).json({ msg: "Id obrigatório" });
+  };
+
+  const user = await prisma.user.findUnique({ where: { id: newId } });
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado" });
+  };
+
+  return res.status(200).json(user);
+
 })
 //#endregion
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+//#region Create User
+app.post('/users', async (req, res) => {
+  const { name, email, password, telefone, cpf } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password,
+      telefone,
+      cpf
+    },
+  });
+  return res.status(201).json(user);
 })
+//#endregion
+
+//#region Update User
+app.put('/users/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, email, password, telefone, cpf } = req.body;
+
+  const newId = parseInt(id);
+
+  if (!newId) {
+    res.status(400).json('Id obrigatório');
+  };
+
+  const userAlreadyExists = await prisma.user.findUnique({ where: { id: newId } });
+
+  if (!userAlreadyExists) {
+    res.status(404).json('Usuário não existente')
+  };
+
+  const user = await prisma.user.update({
+    where: {
+      id: newId
+    },
+    data: {
+      name,
+      email,
+      password,
+      telefone,
+      cpf
+    },
+  });
+
+  return res.status(200).json(user);
+});
+//#endregion
+
+//#region Delete User
+app.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const newId = parseInt(id);
+
+  if (!newId) {
+    return res.status(400).json({ msg: "Id não encontrado" });
+  };
+
+  const userChecker = await prisma.user.findUnique({ where: { id: newId } });
+
+  if (!userChecker) {
+    return res.status(404).json({ msg: 'Usuário não encontrado' })
+  };
+
+  await prisma.user.delete({ where: { id: newId } });
+
+  return res.status(200).send();
+})
+//#endregion
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
 
