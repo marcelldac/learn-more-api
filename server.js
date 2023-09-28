8046d503f1b96b1f0757de8eaef18b81b16eb538
marcelldac <dacmarcell@proton.me>
2023-09-27 22:45:07 -0300

    feat: subjects crud

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const PORT = 3000;
 const userRouter = require('./router/user');
 const healthRouter = require('./router/health');
 const loginRouter = require('./router/login');
+const subjectRouter = require('./router/subject');
 
 app.use(cors());
 app.use(express.json());
 app.use('/users', userRouter);
 app.use('/login', loginRouter);
 app.use('/health', healthRouter);
+app.use('/subjects', subjectRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
