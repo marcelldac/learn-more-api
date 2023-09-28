1c8b673bc34558321178bea51d2cc1cdab099c1b
marcelldac <dacmarcell@proton.me>
2023-09-28 00:24:00 -0300

    feat: teacher crud

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const userRouter = require('./router/user');
 const healthRouter = require('./router/health');
 const loginRouter = require('./router/login');
 const subjectRouter = require('./router/subject');
+const teacherRouter = require('./router/teacher');
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,7 @@ app.use('/users', userRouter);
 app.use('/login', loginRouter);
 app.use('/health', healthRouter);
 app.use('/subjects', subjectRouter);
+app.use('/teachers', teacherRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
